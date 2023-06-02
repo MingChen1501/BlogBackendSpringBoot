c1c88d948592d76c88210fe6c1aa18373944112b
MinhTran <tminh400@gmail.com>
2023-06-03 03:36:47 +0700

    update .gitignore add application.properties, MainClass

---
+++ src/test/java/org/mingchencodelab/blogbackendspringboot/BlogBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mingchencodelab.blogbackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
